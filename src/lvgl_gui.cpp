044896292d00531289369ada9c1fe008487705a7
chenelectrical <103982616+chenelectrical@users.noreply.github.com>
2023-09-20 16:58:56 +0800

    Update lvgl_gui.cpp

--- src/lvgl_gui.cpp
+++ src/lvgl_gui.cpp
@@ -13,7 +13,6 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
 #include <lvgl.h>
-#include <lv_widgets/lv_gauge.h>
 #include <TJpg_Decoder.h>
 #include "qrcode.h"
 #include "lvgl_gui.h"
@@ -32,14 +31,17 @@
 /*                              PRIVATE DATA                                  */
 /******************************************************************************/
 
-static TFT_eSPI tft = TFT_eSPI();
+static TFT_eSPI tft = TFT_eSPI(TFT_WIDTH, TFT_HEIGHT);
 
-lv_obj_t *gauge_co2;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[TFT_WIDTH * TFT_HEIGHT / 10];
+
+lv_obj_t *arc_co2;
+lv_obj_t *label_co2_value;
+lv_obj_t *label_co2_name;
 lv_obj_t *label_temp;
 lv_obj_t *label_pm;
-
-static lv_disp_buf_t disp_buf;
-static lv_color_t buf[LV_HOR_RES_MAX * 10];
+lv_obj_t *label_air;
 
 /******************************************************************************/
 /*                              EXPORTED DATA                                 */
@@ -55,6 +57,7 @@ static void lvgl_task(void *arg);
 static void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
 static void lv_tick_inc_cb(void *data);
 static void lvgl_tick_init(void);
+static void lvgl_ui_init(void);
 
 /******************************************************************************/
 
@@ -62,6 +65,26 @@ static void lvgl_tick_init(void);
  * @brief  Task for update lvgl
  */
 static void lvgl_task(void *arg){
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, TFT_WIDTH * TFT_HEIGHT / 10);
+
+    /* Initialize the display */
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.hor_res = LV_HOR_RES_MAX;
+    disp_drv.ver_res = LV_VER_RES_MAX;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_NONE;
+    lv_indev_drv_register(&indev_drv);
+
+    /* Create UI */
+    lvgl_ui_init();
+
     while(1){
         lv_task_handler();    /* Let the GUI do its work */
         delay(LVGL_TICK_HANDLER);
@@ -111,6 +134,87 @@ static void lvgl_tick_init(void){
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, tick_inc_period_ms * 1000));
 }
 
+/**
+ * @brief  Initialize UI from squareline output
+ */
+static void lvgl_ui_init(void){
+    lv_obj_t *ui_home = lv_scr_act();
+
+    lv_obj_set_style_bg_color(ui_home, lv_color_hex(0x707585), LV_PART_MAIN | LV_STATE_DEFAULT );
+    lv_obj_set_style_bg_opa(ui_home, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_grad_color(ui_home, lv_color_hex(0x455060), LV_PART_MAIN | LV_STATE_DEFAULT );
+
+    arc_co2 = lv_arc_create(ui_home);
+    lv_obj_set_width(arc_co2, 150);
+    lv_obj_set_height(arc_co2, 150);
+    lv_obj_set_x(arc_co2, 0 );
+    lv_obj_set_y(arc_co2, 35 );
+    lv_obj_set_align(arc_co2, LV_ALIGN_TOP_MID );
+    lv_obj_set_style_radius(arc_co2, 350, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_color(arc_co2, lv_color_hex(0x1E232D), LV_PART_MAIN | LV_STATE_DEFAULT );
+    lv_obj_set_style_bg_opa(arc_co2, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_pad_left(arc_co2, 8, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_pad_right(arc_co2, 8, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_pad_top(arc_co2, 8, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_pad_bottom(arc_co2, 8, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_color(arc_co2, lv_color_hex(0x0F1215), LV_PART_MAIN | LV_STATE_DEFAULT );
+    lv_obj_set_style_arc_opa(arc_co2, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_width(arc_co2, 8, LV_PART_MAIN| LV_STATE_DEFAULT);
+
+    lv_obj_set_style_arc_color(arc_co2, lv_color_hex(0x36B9F6), LV_PART_INDICATOR | LV_STATE_DEFAULT );
+    lv_obj_set_style_arc_opa(arc_co2, 255, LV_PART_INDICATOR| LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_width(arc_co2, 8, LV_PART_INDICATOR| LV_STATE_DEFAULT);
+
+    label_co2_value = lv_label_create(arc_co2);
+    lv_obj_set_width( label_co2_value, LV_SIZE_CONTENT);
+    lv_obj_set_height( label_co2_value, LV_SIZE_CONTENT);
+    lv_obj_set_align( label_co2_value, LV_ALIGN_CENTER );
+    lv_label_set_text(label_co2_value,"1000");
+    lv_obj_set_style_text_color(label_co2_value, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT );
+    lv_obj_set_style_text_opa(label_co2_value, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(label_co2_value, &lv_font_montserrat_28, LV_PART_MAIN| LV_STATE_DEFAULT);
+
+    label_co2_name = lv_label_create(arc_co2);
+    lv_obj_set_width( label_co2_name, LV_SIZE_CONTENT);
+    lv_obj_set_height( label_co2_name, LV_SIZE_CONTENT);
+    lv_obj_set_x( label_co2_name, 0 );
+    lv_obj_set_y( label_co2_name, 50 );
+    lv_obj_set_align( label_co2_name, LV_ALIGN_CENTER );
+    lv_label_set_text(label_co2_name,"CO2");
+    lv_obj_set_style_text_color(label_co2_name, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT );
+    lv_obj_set_style_text_opa(label_co2_name, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+
+    label_temp = lv_label_create(ui_home);
+    lv_obj_set_width( label_temp, LV_SIZE_CONTENT);
+    lv_obj_set_height( label_temp, LV_SIZE_CONTENT);
+    lv_obj_set_x( label_temp, -45 );
+    lv_obj_set_y( label_temp, 195 );
+    lv_obj_set_align( label_temp, LV_ALIGN_TOP_MID );
+    lv_label_set_text(label_temp,"T: 36");
+    lv_obj_set_style_text_color(label_temp, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT );
+    lv_obj_set_style_text_opa(label_temp, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+
+    label_pm = lv_label_create(ui_home);
+    lv_obj_set_width( label_pm, LV_SIZE_CONTENT);
+    lv_obj_set_height( label_pm, LV_SIZE_CONTENT);
+    lv_obj_set_x( label_pm, 40 );
+    lv_obj_set_y( label_pm, 195 );
+    lv_obj_set_align( label_pm, LV_ALIGN_TOP_MID );
+    lv_label_set_text(label_pm,"PM: 34");
+    lv_obj_set_style_text_color(label_pm, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT );
+    lv_obj_set_style_text_opa(label_pm, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+
+    label_air = lv_label_create(ui_home);
+    lv_obj_set_width( label_air, LV_SIZE_CONTENT);
+    lv_obj_set_height( label_air, LV_SIZE_CONTENT);
+    lv_obj_set_x( label_air, 0 );
+    lv_obj_set_y( label_air, 10 );
+    lv_obj_set_align( label_air, LV_ALIGN_TOP_MID );
+    lv_label_set_text(label_air,"Air Sensor");
+    lv_obj_set_style_text_color(label_air, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT );
+    lv_obj_set_style_text_opa(label_air, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+}
+
 /******************************************************************************/
 
 /**
@@ -118,58 +222,12 @@ static void lvgl_tick_init(void){
  */
 void lvgl_gui_sensor_start(void){
     lv_init();
-    lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * 10);
     lvgl_tick_init();
-
-    /*Initialize the display*/
-    lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    disp_drv.hor_res = LV_HOR_RES_MAX;
-    disp_drv.ver_res = LV_VER_RES_MAX;
-    disp_drv.flush_cb = my_disp_flush;
-    disp_drv.buffer = &disp_buf;
-    lv_disp_drv_register(&disp_drv);
-
-    /*Initialize the (dummy) input device driver*/
-    lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_NONE;
-    lv_indev_drv_register(&indev_drv);
-
-    /* Describe the color for the needles */
-    static lv_color_t needle_colors[3];
-    needle_colors[0] = LV_COLOR_BLUE;
-    needle_colors[1] = LV_COLOR_RED;
-    needle_colors[2] = LV_COLOR_GREEN;
 
     /* Clear LCD */
     tft.fillScreen(TFT_BLACK);
 
-    lv_obj_t *label = lv_label_create(lv_scr_act(), NULL);
-    lv_label_set_text(label, "Air Sensor");
-    lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_MID, 0, 10);
-
-    /* Gauge CO2 */
-    gauge_co2 = lv_gauge_create(lv_scr_act(), NULL);
-    lv_gauge_set_needle_count(gauge_co2, 1, &needle_colors[1]);
-    lv_obj_set_size(gauge_co2, 170, 170);
-    lv_obj_align(gauge_co2, NULL, LV_ALIGN_IN_TOP_MID, 0, 30);
-    lv_gauge_set_range(gauge_co2, 0, CO2_MAX);
-    lv_gauge_set_scale(gauge_co2, 270, 17, 5);
-
-    lv_obj_t *label_co2 = lv_label_create(gauge_co2, NULL);
-    lv_label_set_text(label_co2, "CO2");
-    lv_obj_align(label_co2, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -10);
-
-    label_temp = lv_label_create(lv_scr_act(), NULL);
-    lv_label_set_text(label_temp, "T: 20");
-    lv_obj_align(label_temp, NULL, LV_ALIGN_IN_TOP_LEFT, 70, 205);
-
-    label_pm = lv_label_create(lv_scr_act(), NULL);
-    lv_label_set_text(label_pm, "PM: 123");
-    lv_obj_align(label_pm, NULL, LV_ALIGN_IN_TOP_LEFT, 120, 205);
-
-    xTaskCreate(lvgl_task, "LVGL", 4096, NULL, 4, NULL);
+    xTaskCreate(lvgl_task, "LVGL", 10240, NULL, 4, NULL);
 }
 
 /**
@@ -184,9 +242,7 @@ void lvgl_gui_print(int co2, float temp, int pm2){
     int co2_value = (co2 > CO2_MAX) ? CO2_MAX : co2;
     int pm_value = (pm2 > PM2_MAX) ? PM2_MAX : pm2;
 
-    /* Display LCD */
-    lv_gauge_set_value(gauge_co2, 0, co2_value);
-
+    lv_arc_set_value(arc_co2, co2_value * 100 / CO2_MAX);
     sprintf(temp_str, "T: %d", temp_value);
     lv_label_set_text(label_temp, temp_str);
     sprintf(pm_str, "PM: %d", pm2);
